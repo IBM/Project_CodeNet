4f9ad5fc08c4275fb06dd18a26370ce5fd6e7adf
Ulrich Finkler <ufinkler@us.ibm.com>
2021-05-18 11:07:31 -0400

    Removed

--- model-experiments/token-based-classification-similarity/run/cpp1000/tokenize/tokenize.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#Tokenizing C++ benchmark dataset
-#with 1000 problems with 500 solution source code files each
-#Parameters:
-# $1  -- path to benchmark dataset with source code files
-SRC=../../../src
-python $SRC/DSMaker/TokenizeImportDS.py ./cpp1000ds --source $1 --language C++ --tok_set CPP56X --update --no_macro
-
